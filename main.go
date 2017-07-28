4c0cf6246e5ee014fe65e7e998c56c72d58b5ba9
Matt Whiteley <mwhiteley@fastly.com>
2017-07-28 10:19:54 -0700

    newSplitScanner

--- main.go
+++ main.go
@@ -3,12 +3,18 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
 )
 
-func main() {
+func newSplitScanner(r io.Reader, split bufio.SplitFunc) *bufio.Scanner {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Split(bufio.ScanWords)
+	return scanner
+}
+
+func main() {
+	scanner := newSplitScanner(os.Stdin, bufio.ScanWords)
 	count := make(map[string]int)
 	for scanner.Scan() {
 		word := scanner.Text()
